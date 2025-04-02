e152956f093fad6c048d9bbbc78358dbb562cee6
PB <xachineon@gmail.com>
2025-04-02 18:34:20 +0530

    Completable demo included.

---
+++ src/main/java/com/thread/learn/threadings/Async/Completable/CompletableFutureDemo1.java
@@ -0,0 +1,50 @@
+package com.thread.learn.threadings.Async.Completable;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class CompletableFutureDemo1 {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // Retrieve number of points from a remote database call
+        CompletableFuture<Integer> future = CompletableFuture.supplyAsync(
+                new Supplier<Integer>() {
+                    public Integer get() {
+                        System.out.println("Accessing customer loyalty points data over the network...");
+                        try {
+                            TimeUnit.SECONDS.sleep(4);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return 200;
+                    }
+                });
+
+        CompletableFuture<Integer> futureLambda = CompletableFuture.supplyAsync(
+                () -> {
+                    System.out.println("Accessing customer loyalty points data over the network by lambda...");
+                    try {
+                        TimeUnit.SECONDS.sleep(4);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    return 200;
+                });
+        // Attach a callback to the Future using thenApply()
+        CompletableFuture<Integer> futureWithCallback = future.thenApply(
+                result -> {
+                    int totalPoints = result;
+                    if (result >= 10 && result <= 100) {
+                        totalPoints += 10;
+                    } else if (result >= 101 && result <= 200) {
+                        totalPoints += 20;
+                    }
+                    return totalPoints;
+                });
+        // Manually completing with the last cached data
+        futureWithCallback.complete(150);
+
+        System.out.println("Total points:: " + futureWithCallback.get());
+    }
+}
